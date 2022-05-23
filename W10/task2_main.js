9fe2bd2a4d4ecd9e8e23f5cc2315d4d4e9b567dc
lNORIKAl <103481176+lNORIKAl@users.noreply.github.com>
2022-05-23 20:56:33 +0900

    Add files via upload

---
+++ W10/task2_main.js
@@ -0,0 +1,123 @@
+d3.csv("https://lNORIKAl.github.io/InfoVis2022/W06/w06_task1.csv")
+    .then(data => {
+        data.forEach(d => { d.x = +d.x; d.y = +d.y; });
+
+        var config = {
+            parent: '#drawing_region',
+            width: 256 + 50,
+            height: 256 + 50,
+            margin: { top: 30, right: 10, bottom: 20, left: 30 },
+            xticks: 10,
+            yticks: 10,
+            padrate: 0.2,
+        };
+
+        const scatter_plot = new ScatterPlot(config, data);
+        scatter_plot.update();
+    })
+    .catch(error => {
+        console.log(error);
+    });
+
+function padrange(vmin, vmax, rate) {
+    var len = vmax - vmin;
+
+    return [vmin - rate * len, vmax + rate * len];
+}
+
+class ScatterPlot {
+
+    constructor(config, data) {
+        this.config = {
+            parent: config.parent,
+            width: config.width || 256,
+            height: config.height || 256,
+            margin: config.margin || { top: 10, right: 10, bottom: 10, left: 10 },
+            xticks: config.xticks || 10,
+            yticks: config.yticks || 10,
+            padrate: config.padrate || 10,
+        }
+        this.data = data;
+        this.init();
+    }
+
+    init() {
+        let self = this;
+
+        self.svg = d3.select(self.config.parent)
+            .attr('width', self.config.width)
+            .attr('height', self.config.height);
+
+        self.chart = self.svg.append('g')
+            .attr('transform', `translate(${self.config.margin.left}, ${self.config.margin.top})`);
+
+        self.inner_width = self.config.width - self.config.margin.left - self.config.margin.right;
+        self.inner_height = self.config.height - self.config.margin.top - self.config.margin.bottom;
+
+        self.xscale = d3.scaleLinear()
+            .range([0, self.inner_width]);
+
+        self.yscale = d3.scaleLinear()
+            .range([0, self.inner_height]);
+
+        self.xaxis = d3.axisBottom(self.xscale)
+            .ticks(self.config.xticks);
+
+        self.yaxis = d3.axisLeft(self.yscale)
+            .ticks(self.config.yticks)
+
+        self.xaxis_group = self.chart.append('g')
+            .attr('transform', `translate(0, ${self.inner_height})`);
+
+        self.yaxis_group = self.chart.append('g')
+            .attr('transform', `translate(0, 0)`);
+    }
+
+    update() {
+        let self = this;
+
+        const xmin = d3.min(self.data, d => d.x);
+        const xmax = d3.max(self.data, d => d.x);
+        // self.xscale.domain([xmin, xmax]);
+        self.xscale.domain(padrange(xmin, xmax, self.config.padrate));
+
+        const ymin = d3.min(self.data, d => d.y);
+        const ymax = d3.max(self.data, d => d.y);
+        self.yscale.domain(padrange(ymin, ymax, self.config.padrate));
+
+        self.render();
+    }
+
+    render() {
+        let self = this;
+
+        self.chart.selectAll("circle")
+            .data(self.data)
+            .enter()
+            .append("circle")
+            .attr("cx", d => self.xscale(d.x))
+            .attr("cy", d => self.yscale(d.y))
+            .attr("r", d => d.r)
+            .on('mouseover', (e,d) => {
+        		d3.select('#tooltip')
+            	  .style('opacity', 1)
+                  .html(`<div class="tooltip-label">Position</div>(${d.x}, ${d.y})`);
+    		 })
+		    .on('mousemove', (e) => {
+		        const padding = 10;
+		        d3.select('#tooltip')
+		            .style('left', (e.pageX + padding) + 'px')
+		            .style('top', (e.pageY + padding) + 'px');
+		    })
+		    .on('mouseleave', () => {
+		        d3.select('#tooltip')
+		            .style('opacity', 0);
+		    });
+
+        self.xaxis_group
+            .call(self.xaxis);
+
+        self.yaxis_group
+            .call(self.yaxis)
+    }
+}
